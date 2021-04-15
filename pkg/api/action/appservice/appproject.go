e886b6b12da919fbc7d0bf9b858f0121e82437ee
Johnathan <695302091@qq.com>
2021-04-15 14:45:03 +0800

    add appProject api

--- pkg/api/action/appservice/appproject.go
+++ pkg/api/action/appservice/appproject.go
@@ -8,8 +8,9 @@ import (
 )
 
 func (s *Server) ListAppProject(g *gin.Context) {
+	search := g.Query("search")
 
-	data, count,  err := s.AppProjectService.List(1, 10)
+	data, count, err := s.AppProjectService.List(search)
 	if err != nil {
 		api.RequestParamsError(g, "error", err)
 		return
@@ -17,9 +18,44 @@ func (s *Server) ListAppProject(g *gin.Context) {
 	g.JSON(http.StatusOK, gin.H{"data": data, "count": count})
 }
 
-func (s *Server) CreateAppProject(g *gin.Context)  {
-	var a resource.AppProject
-	g.ShouldBindJSON(&a)
-	s.AppProjectService.Create(&a)
-	g.JSON(http.StatusOK, gin.H{"data": a})
+func (s *Server) CreateAppProject(g *gin.Context) {
+	var app resource.AppProject
+	if err := g.ShouldBindJSON(&app); err != nil {
+		api.RequestParamsError(g, "error", err)
+	}
+	if err := s.AppProjectService.Create(&app); err != nil {
+		api.RequestParamsError(g, "error", err)
+	}
+	g.JSON(http.StatusOK, gin.H{"data": app})
+}
+
+func (s *Server) RetrieveAppProject(g *gin.Context) {
+	uuid := g.Param("uuid")
+	data, err := s.AppProjectService.GetByUUID(uuid)
+	if err != nil {
+		api.RequestParamsError(g, "error", err)
+		return
+	}
+	g.JSON(http.StatusOK, data)
+}
+
+func (s *Server) UpdateAppProject(g *gin.Context) {
+	uuid := g.Param("uuid")
+	var app resource.AppProject
+	if err := g.ShouldBindJSON(&app); err != nil {
+		api.RequestParamsError(g, "error", err)
+	}
+	data, update, err := s.AppProjectService.Update(uuid, &app)
+	if err != nil {
+		api.RequestParamsError(g, "error", err)
+	}
+	g.JSON(http.StatusOK, gin.H{"data": data, "update": update})
+}
+
+func (s *Server) DeleteAppProject(g *gin.Context) {
+	uuid := g.Param("uuid")
+	if err := s.AppProjectService.Delete(uuid); err != nil {
+		api.RequestParamsError(g, "error", err)
+	}
+	g.JSON(http.StatusOK, gin.H{"delete": true})
 }
