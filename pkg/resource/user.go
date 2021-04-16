beadfa91d312d2ad0c30ee16093e0c7f0bbc6d3f
yoshiaki <yoshiakiley@gmail.com>
2021-04-16 13:53:04 +0800

    fix appproject and add mongo watch method

--- pkg/resource/user.go
+++ pkg/resource/user.go
@@ -2,7 +2,11 @@ package resource
 
 import (
 	"github.com/yametech/devops/pkg/core"
+	"github.com/yametech/devops/pkg/store"
+	"github.com/yametech/devops/pkg/store/gtm"
 )
+
+const UserKind core.Kind = "user"
 
 type UserSpec struct {
 	Password    string `json:"password" bson:"password"`
@@ -21,4 +25,17 @@ func (u *User) Clone() core.IObject {
 	result := &User{}
 	core.Clone(u, result)
 	return result
+}
+
+// User impl Coder
+func (*User) Decode(op *gtm.Op) (core.IObject, error) {
+	user := &User{}
+	if err := core.ObjectToResource(op.Data, user); err != nil {
+		return nil, err
+	}
+	return user, nil
+}
+
+func init() {
+	store.AddResourceCoder(string(UserKind), &User{})
 }
--- pkg/utils/tools.go
+++ pkg/utils/tools.go
@@ -2,15 +2,10 @@ package utils
 
 import "encoding/json"
 
-func Clone(src interface{}, dst interface{}) error {
+func UnstructuredObjectToInstanceObj(src interface{}, dst interface{}) error {
 	data, err := json.Marshal(src)
 	if err != nil {
 		return err
 	}
-	err = json.Unmarshal(data, dst)
-	if err != nil {
-		return err
-	}
-
-	return nil
+	return json.Unmarshal(data, dst)
 }
