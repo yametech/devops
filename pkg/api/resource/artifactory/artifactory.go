415bbe58c63eb3086978d2cbd34cd74f0604ce8e
zyyong <zhang103886108@163.com>
2021-04-21 17:26:21 +0800

    add artifactory server
    * 制品库代码
    
    * 修改方法调用逻辑
    
    Co-authored-by: zhangzhenyong <zhangzhenyong@ecpark.cn>

--- pkg/api/resource/artifactory/artifactory.go
+++ pkg/api/resource/artifactory/artifactory.go
@@ -2,7 +2,9 @@ package artifactory
 
 import "github.com/yametech/devops/pkg/core"
 
-type RequestArtifactory struct {
+type ArtifactStatus uint8
+
+type RequestArtifact struct {
 	GitUrl    string `json:"git_url"`
 	AppName   string `json:"app_name"`
 	Branch    string `json:"branch"`
@@ -12,18 +14,29 @@ type RequestArtifactory struct {
 	ImagesHub string `json:"images_hub"`
 }
 
-type ArtifactorySpec struct {
-	ImagesUrl string `json:"images_url"`
-	GitUrl    string `json:"git_url"`
-	AppName   string `json:"app_name"`
-	Branch    string `json:"branch"`
-	Tag       string `json:"tag"`
-	Remarks   string `json:"remarks"`
-	Language  string `json:"language"`
-	ImagesHub string `json:"images_hub"`
+type ArtifactSpec struct {
+	ImagesUrl      string `json:"images_url"`
+	GitUrl         string `json:"git_url"`
+	AppName        string `json:"app_name"`
+	Branch         string `json:"branch"`
+	Tag            string `json:"tag"`
+	Remarks        string `json:"remarks"`
+	Language       string `json:"language"`
+	ImagesHub      string `json:"images_hub"`
+	ArtifactStatus `json:"artifact_status" bson:"artifact_status"`
 }
 
-type ResArtifactory struct {
+type RespArtifact struct {
 	core.Metadata `json:"metadata"`
-	Spec          ArtifactorySpec `json:"spec"`
+	Spec          ArtifactSpec `json:"spec"`
+}
+
+type Metadata struct {
+	Name        string                 `json:"name" bson:"name"`
+	Kind        string                 `json:"kind"  bson:"kind"`
+	UUID        string                 `json:"uuid" bson:"uuid"`
+	Version     int64                  `json:"version" bson:"version"`
+	IsDelete    bool                   `json:"is_delete" bson:"is_delete"`
+	CreatedTime int64                  `json:"created_time" bson:"created_time"`
+	Labels      map[string]interface{} `json:"labels" bson:"labels"`
 }
--- pkg/resource/artifactory/artifact.go
+++ pkg/resource/artifactory/artifact.go
@@ -6,6 +6,8 @@ import (
 
 type ArtifactStatus uint8
 
+const ArtifactKind core.Kind = "artifact"
+
 const (
 	Created ArtifactStatus = iota
 	Building
@@ -15,6 +17,7 @@ const (
 
 type ArtifactSpec struct {
 	ArtifactStatus `json:"artifact_status" bson:"artifact_status"`
+	Branch         string `json:"branch" bson:"branch"`
 	GitUrl         string `json:"git_url" bson:"git_url"`
 	Language       string `json:"language" bson:"language"`
 	Tag            string `json:"tag" bson:"tag" `
@@ -22,6 +25,7 @@ type ArtifactSpec struct {
 	AppConfig      string `json:"app_config" bson:"app_config"` // gitUrl和Language存在全局配置中，不需要保存在这里
 	AppName        string `json:"app_name" bson:"app_name"`     // 只存英文名，appCode不需要，用name搜索
 	CreateUserId   string `json:"create_user_id" bson:"create_user_id"`
+	Remarks        string `json:"remarks" bson:"remarks"`
 }
 
 type Artifact struct {
