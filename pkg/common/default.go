864ebcbeeebf8ae70307ead1ba64ffd89d1c4b96
Johnathan <695302091@qq.com>
2021-04-21 16:50:28 +0800

    megre upstream

--- pkg/common/default.go
+++ pkg/common/default.go
@@ -1,10 +1,25 @@
 package common
 
+import "flag"
+
 const (
 	DefaultNamespace = "devops"
 	User             = "user"
 	UserProject      = "userproject"
-	AppProject       = "appproject"
+	EchoerUrl        = "http://10.200.65.192:8080"
+	Artifactory      = "artifactory"
 	AppConfig        = "appconfig"
+	AppProject       = "appproject"
+	GlobalConfig     = "globalconfig"
 	WorkOrder        = "workorder"
 )
+
+var (
+	EchoerCI = "artifactoryCI"
+	EchoerCD = "artifactoryCD"
+)
+
+func init() {
+	flag.StringVar(&EchoerCI, "echoerci", "artifactoryCI", "-echoerci = artifactoryCI")
+	flag.StringVar(&EchoerCD, "echoercd", "artifactoryCD", "-echoercd = artifactoryCD")
+}
