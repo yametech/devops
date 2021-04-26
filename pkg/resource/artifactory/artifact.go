3d9f23f237754d6d0705d389ccd158db21558c87
yoshiaki <yoshiakiley@gmail.com>
2021-04-26 15:22:24 +0800

    Artifact add some key

--- pkg/resource/artifactory/artifact.go
+++ pkg/resource/artifactory/artifact.go
@@ -21,9 +21,11 @@ type ArtifactSpec struct {
 	GitUrl         string `json:"git_url" bson:"git_url"`
 	Language       string `json:"language" bson:"language"`
 	Tag            string `json:"tag" bson:"tag" `
-	Images         string `json:"images" bson:"images"`
-	AppConfig      string `json:"app_config" bson:"app_config"` // gitUrl和Language存在全局配置中，不需要保存在这里
-	AppName        string `json:"app_name" bson:"app_name"`     // 只存英文名，appCode不需要，用name搜索
+	Registry       string `json:"registry" bson:"registry"`         // 镜像库地址
+	Images         string `json:"images" bson:"images"`             // CI的镜像完整路径
+	ProjectPath    string `json:"project_path" bson:"project_path"` // 项目CI时WORKDIR
+	ProjectFile    string `json:"project_file" bson:"project_file"` // 项目CI时Dockerfile路径
+	AppName        string `json:"app_name" bson:"app_name"`         // 只存英文名，appCode不需要，用name搜索
 	CreateUserId   string `json:"create_user_id" bson:"create_user_id"`
 	Remarks        string `json:"remarks" bson:"remarks"`
 }
