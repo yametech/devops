3efba6651576dbc7264f665b7ec92001ab795fe1
zhangzhenyong <zhangzhenyong@ecpark.cn>
2021-04-26 16:23:18 +0800

    获取commit hash

--- pkg/service/artifactory/artifactory.go
+++ pkg/service/artifactory/artifactory.go
@@ -11,6 +11,7 @@ import (
 	"github.com/yametech/devops/pkg/core"
 	arResource "github.com/yametech/devops/pkg/resource/artifactory"
 	"github.com/yametech/devops/pkg/service"
+	"github.com/yametech/devops/pkg/utils"
 	"github.com/yametech/go-flowrun"
 	"go.mongodb.org/mongo-driver/bson"
 	"go.mongodb.org/mongo-driver/bson/primitive"
@@ -56,20 +57,8 @@ func (a *ArtifactService) List(name string, page, pageSize int64) ([]interface{}
 }
 
 func (a *ArtifactService) Create(reqAr *apiResource.RequestArtifact) error {
-	gitPath := ""
-	if strings.Contains(reqAr.GitUrl, "http://") {
-		sliceTemp := strings.Split(reqAr.GitUrl, "http://")
-		gitPath = sliceTemp[len(sliceTemp)-1]
-	} else if strings.Contains(reqAr.GitUrl, "https://") {
-		sliceTemp := strings.Split(gitPath, "https://")
-		gitPath = sliceTemp[len(sliceTemp)-1]
-	}
-
 	if len(reqAr.Tag) == 0 {
-		commithash, err := GetHeadHash(gitPath)
-		if err != nil {
-			reqAr.Tag = commithash
-		}
+		reqAr.Tag = utils.NewSUID().String()
 	}
 	ar := &arResource.Artifact{
 		Spec: arResource.ArtifactSpec{
@@ -177,19 +166,4 @@ func (a *ArtifactService) GetBanch(gitpath string) ([]string, error) {
 		return nil
 	})
 	return sliceBranch, err
-}
-
-func GetHeadHash(gitpath string) (string, error) {
-	url := fmt.Sprintf("http://%s:%s@%s", common.GitUser, common.GitPW, gitpath)
-	r, _ := git.Clone(memory.NewStorage(), nil, &git.CloneOptions{
-		URL:          url,
-		SingleBranch: false,
-		NoCheckout:   true,
-		Depth:        1,
-	})
-	ref, err := r.Head()
-	if ref != nil {
-		return ref.Hash().String(), err
-	}
-	return "", err
 }
