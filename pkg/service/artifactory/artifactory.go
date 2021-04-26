943adf08de1192090369fe6397ddb0d31651b357
Sstylle <786697810@qq.com>
2021-04-26 21:30:58 +0800

    artifactory: create artifactory to echoer and format respond of the create

--- pkg/service/artifactory/artifactory.go
+++ pkg/service/artifactory/artifactory.go
@@ -1,6 +1,7 @@
 package artifactory
 
 import (
+	"encoding/json"
 	"fmt"
 	"github.com/go-git/go-git/v5"
 	"github.com/go-git/go-git/v5/plumbing"
@@ -14,6 +15,7 @@ import (
 	"github.com/yametech/go-flowrun"
 	"go.mongodb.org/mongo-driver/bson"
 	"go.mongodb.org/mongo-driver/bson/primitive"
+	"strconv"
 	"strings"
 	"time"
 )
@@ -37,7 +39,7 @@ func (a *ArtifactService) List(name string, page, pageSize int64) ([]interface{}
 	offset := (page - 1) * pageSize
 	filter := map[string]interface{}{}
 	if name != "" {
-		filter["metadata.name"] = bson.M{"$regex": primitive.Regex{Pattern: ".*" + name + ".*", Options: "i"}}
+		filter["spec.app_name"] = bson.M{"$regex": primitive.Regex{Pattern: ".*" + name + ".*", Options: "i"}}
 	}
 	sort := map[string]interface{}{
 		"metadata.version": -1,
@@ -56,9 +58,6 @@ func (a *ArtifactService) List(name string, page, pageSize int64) ([]interface{}
 }
 
 func (a *ArtifactService) Create(reqAr *apiResource.RequestArtifact) error {
-
-	
-
 	gitPath := ""
 	if strings.Contains(reqAr.GitUrl, "http://") {
 		sliceTemp := strings.Split(reqAr.GitUrl, "http://")
@@ -68,19 +67,43 @@ func (a *ArtifactService) Create(reqAr *apiResource.RequestArtifact) error {
 		gitPath = sliceTemp[len(sliceTemp)-1]
 	}
 
+	gitDirectory := ""
+	if strings.Contains(gitPath, "/") {
+		if sliceTemp := strings.Split(gitPath, "/"); len(sliceTemp) > 2 {
+			gitDirectory = sliceTemp[len(sliceTemp)-2]
+		}
+	}
+
+	registry := ""
+	if strings.Contains(reqAr.Registry, "http://") {
+		sliceTemp := strings.Split(reqAr.Registry, "http://")
+		registry = sliceTemp[len(sliceTemp)-1]
+	} else if strings.Contains(reqAr.Registry, "https://") {
+		sliceTemp := strings.Split(gitPath, "https://")
+		registry = sliceTemp[len(sliceTemp)-1]
+	}
+	registry = fmt.Sprintf("%s/%s", registry, gitDirectory)
+
 	if len(reqAr.Tag) == 0 {
 		reqAr.Tag = utils.NewSUID().String()
-
 	}
+
+	appName := fmt.Sprintf("%s-%d", reqAr.AppName, a.GetAppNumber(reqAr.AppName)+1)
+
 	ar := &arResource.Artifact{
+		Metadata: core.Metadata{
+			Name: appName,
+		},
 		Spec: arResource.ArtifactSpec{
-			GitUrl:   reqAr.GitUrl,
-			AppName:  reqAr.AppName,
-			Branch:   reqAr.Branch,
-			Tag:      reqAr.Tag,
-			Remarks:  reqAr.Remarks,
-			Language: reqAr.Language,
-			Registry: reqAr.Registry,
+			GitUrl:      reqAr.GitUrl,
+			AppName:     reqAr.AppName,
+			Branch:      reqAr.Branch,
+			Tag:         reqAr.Tag,
+			Remarks:     reqAr.Remarks,
+			Language:    reqAr.Language,
+			Registry:    registry,
+			ProjectFile: reqAr.ProjectFile,
+			ProjectPath: reqAr.ProjectPath,
 		},
 	}
 
@@ -90,12 +113,25 @@ func (a *ArtifactService) Create(reqAr *apiResource.RequestArtifact) error {
 		return err
 	}
 	//TODO:sendCIEcho
-	//arCIInfo := &arResource.ArtifactCIInfo{}
-	//sendCIInfo, err := core.ToMap(arCIInfo)
-	//if SendEchoer(ar.Metadata.UUID, common.EchoerCI, sendCIInfo) {
-	//	//TODO:change CIStatus
-	//	return nil
-	//}
+	arCIInfo := &arResource.ArtifactCIInfo{
+		Branch:      reqAr.Branch,
+		CodeType:    reqAr.Language,
+		CommitID:    reqAr.Tag,
+		GitUrl:      reqAr.GitUrl,
+		OutPut:      registry,
+		ProjectPath: reqAr.ProjectPath,
+		ProjectFile: reqAr.ProjectFile,
+		RetryCount:  15,
+		ServiceName: reqAr.AppName,
+	}
+	sendCIInfo, err := core.ToMap(arCIInfo)
+	if err != nil {
+		return err
+	}
+	if SendEchoer(ar.Metadata.UUID, common.EchoerCI, sendCIInfo) {
+		//TODO:change CIStatus
+		return nil
+	}
 	return nil
 }
 
@@ -146,7 +182,7 @@ func SendEchoer(stepName string, actionName string, a map[string]interface{}) bo
 		"SUCCESS": "done", "FAIL": "done",
 	}
 
-	flowRunStepName := fmt.Sprintf("%s_%s", common.DefaultNamespace, stepName)
+	flowRunStepName := fmt.Sprintf("%s_%s", actionName, "4444")
 	flowRun.AddStep(flowRunStepName, flowRunStep, actionName, a)
 
 	flowRunData := flowRun.Generate()
@@ -160,16 +196,19 @@ func SendEchoer(stepName string, actionName string, a map[string]interface{}) bo
 
 func (a *ArtifactService) GetBranch(gitpath string) ([]string, error) {
 	url := fmt.Sprintf("http://%s:%s@%s", common.GitUser, common.GitPW, gitpath)
-	r, _ := git.Clone(memory.NewStorage(), nil, &git.CloneOptions{
+	r, err := git.Clone(memory.NewStorage(), nil, &git.CloneOptions{
 		URL:          url,
 		SingleBranch: false,
 		NoCheckout:   true,
 		Depth:        1,
 	})
+	if err != nil {
+		return nil, err
+	}
 
 	sliceBranch := make([]string, 0)
 	referenceIter, _ := r.References()
-	err := referenceIter.ForEach(func(c *plumbing.Reference) error {
+	err = referenceIter.ForEach(func(c *plumbing.Reference) error {
 		if strings.Contains(string(c.Name()), "refs/remotes/origin/") {
 			sliceTemp := strings.Split(string(c.Name()), "refs/remotes/origin/")
 			sliceBranch = append(sliceBranch, sliceTemp[len(sliceTemp)-1])
@@ -177,4 +216,30 @@ func (a *ArtifactService) GetBranch(gitpath string) ([]string, error) {
 		return nil
 	})
 	return sliceBranch, err
+}
 
+func (a *ArtifactService) GetAppNumber(appName string) int {
+	data, _, err := a.List(appName, 1, 0)
+	if err != nil {
+		return 0
+	}
+	b, err := json.Marshal(data)
+	c := make([]*arResource.Artifact, 0)
+	err = json.Unmarshal(b, &c)
+	if err != nil {
+		fmt.Println(err)
+		return 0
+	}
+	var number = 0
+	for _, v := range c {
+		sliceName := strings.Split(v.Metadata.Name, "-")
+		i, _ := strconv.Atoi(sliceName[len(sliceName)-1])
+		if err != nil {
+			return 0
+		}
+		if i > number {
+			number = i
+		}
+	}
+	return number
+}
