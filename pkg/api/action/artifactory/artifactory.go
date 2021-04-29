22044c9e79400264f77291633d44e2067f63833e
zyyong <zhang103886108@163.com>
2021-04-29 14:03:26 +0800

    根据gittea的api，修改获取分支的逻辑 (#45)
    * 制品库代码
    
    * 修改方法调用逻辑
    
    * 根据git获取分支的逻辑
    
    * 合并代码
    
    * 修改git获取分支逻辑
    
    * 修改git获取分支逻辑
    
    * 获取commit hash
    
    * 获取commit hash
    
    * 获取gittea 分支
    
    * 根据git获取分支的逻辑
    
    * 修改git获取分支逻辑
    
    * 获取gittea 分支
    
    * 获取gittea 分支
    
    * 获取gittea 分支
    
    Co-authored-by: zhangzhenyong <zhangzhenyong@ecpark.cn>
    Co-authored-by: YoshiAki Ley <38936845+yoshiakiley@users.noreply.github.com>

--- pkg/api/action/artifactory/artifactory.go
+++ pkg/api/action/artifactory/artifactory.go
@@ -47,10 +47,10 @@ func (b *baseServer) ListArtifact(g *gin.Context) {
 		api.RequestParamsError(g, "error", err)
 		return
 	}
-
 	data := map[string]interface{}{"results": results}
 	data["count"] = count
-	api.ResponseSuccess(g, data, "")
+	api.ResponseSuccess(g, data)
+	//g.JSON(http.StatusOK, map[string]interface{}{"data": results})
 }
 
 func (b *baseServer) CreateArtifact(g *gin.Context) {
@@ -65,12 +65,12 @@ func (b *baseServer) CreateArtifact(g *gin.Context) {
 		return
 	}
 
-	res, err := b.ArtifactService.Create(request)
+	err = b.ArtifactService.Create(request)
 	if err != nil {
-		api.RequestParamsError(g, "create artifact error", err)
+		api.RequestParamsError(g, "create user error", err)
 		return
 	}
-	api.ResponseSuccess(g, res, "")
+	g.JSON(http.StatusOK, request)
 }
 
 func (b *baseServer) GetArtifact(g *gin.Context) {
@@ -80,9 +80,7 @@ func (b *baseServer) GetArtifact(g *gin.Context) {
 		api.RequestParamsError(g, "error", err)
 		return
 	}
-
-	result := map[string]interface{}{"results": data}
-	api.ResponseSuccess(g, result, "")
+	g.JSON(http.StatusOK, data)
 }
 
 func (b *baseServer) DeleteArtifact(g *gin.Context) {
@@ -96,7 +94,7 @@ func (b *baseServer) DeleteArtifact(g *gin.Context) {
 		api.RequestParamsError(g, "delete fail", err)
 		return
 	}
-	api.ResponseSuccess(g, nil, "删除成功")
+	g.JSON(http.StatusOK, nil)
 }
 
 func (b *baseServer) UpdateArtifact(g *gin.Context) {
@@ -125,21 +123,20 @@ func (b *baseServer) UpdateArtifact(g *gin.Context) {
 
 func (b *baseServer) GetBranchList(g *gin.Context) {
 	gitPath := g.Query("gitpath")
-
-	if strings.Contains(gitPath, "http://") {
-		sliceTemp := strings.Split(gitPath, "http://")
-		gitPath = sliceTemp[len(sliceTemp)-1]
-	} else if strings.Contains(gitPath, "https://") {
-		sliceTemp := strings.Split(gitPath, "https://")
-		gitPath = sliceTemp[len(sliceTemp)-1]
+	gitPath = strings.Replace(gitPath, ".git", "", -1)
+	sliceTemp := strings.Split(gitPath, "/")
+	org, name := "", ""
+	if len(sliceTemp) >= 2 {
+		org = sliceTemp[len(sliceTemp)-2]
+		name = sliceTemp[len(sliceTemp)-1]
+	} else {
+		return
 	}
-	results, err := b.ArtifactService.GetBranch(gitPath)
+
+	results, err := b.ArtifactService.GetBanch(org, name)
 	if err != nil {
 		api.RequestParamsError(g, "error", err)
 		return
 	}
-
-	data := map[string]interface{}{"results": results}
-	data["count"] = len(results)
-	api.ResponseSuccess(g, data, "")
+	g.JSON(http.StatusOK, map[string]interface{}{"code": http.StatusOK, "data": results})
 }
