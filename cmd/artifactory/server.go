08e7cb3f01e19c099d2a7da72a3c9f1433be8d85
YoshiAki Ley <38936845+yoshiakiley@users.noreply.github.com>
2021-05-12 16:07:38 +0800

    delete base user and fix gosum bug (#64)

--- cmd/artifactory/server.go
+++ cmd/artifactory/server.go
@@ -4,8 +4,10 @@ import (
 	"flag"
 	"github.com/yametech/devops/pkg/api"
 	"github.com/yametech/devops/pkg/api/action/artifactory"
+	"github.com/yametech/devops/pkg/common"
 	"github.com/yametech/devops/pkg/service"
 	"github.com/yametech/devops/pkg/store/mongo"
+	"github.com/yametech/go-insect"
 )
 
 var storageUri string
@@ -21,6 +23,12 @@ func main() {
 
 	baseService := service.NewBaseService(store)
 	server := api.NewServer(baseService)
+
+	//注册网关
+	go_insect.GlobalEtcdAddress = common.EtcdAddress
+	go_insect.INSECT_SERVER_PORT = 8080
+	go_insect.INSECT_SERVER_NAME = "artifactory"
+	go go_insect.EtcdProxy()
 
 	artifactory.NewArBaseServer("artifactory", server)
 
