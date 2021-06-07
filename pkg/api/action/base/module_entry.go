0ee6cb110d96d8c49d858e6d39a86c9ae3f5bc2e
Weihengxing <2358343165@qq.com>
2021-06-07 14:40:45 +0800

    module_entry-reconsitution

--- pkg/api/action/base/module_entry.go
+++ pkg/api/action/base/module_entry.go
@@ -2,47 +2,24 @@ package base
 
 import (
 	"github.com/gin-gonic/gin"
-	"github.com/pkg/errors"
 	"github.com/yametech/devops/pkg/api"
-	"strconv"
 )
 
 func (b *baseServer) CreateModuleEntry(c *gin.Context) {
 	user := c.Request.Header.Get("x-wrapper-username")
 	uuid := c.Query("uuid")
-	page, err := strconv.ParseInt(c.DefaultQuery("page", "1"), 10, 64)
-	if err != nil {
-		api.ResponseError(c, errors.New("page need int type"))
-		return
-	}
-	pageSize, err := strconv.ParseInt(c.DefaultQuery("page_size", "10"), 10, 64)
-	if err != nil {
-		api.ResponseError(c, errors.New("pageSize need int type"))
-		return
-	}
-	response, err := b.CreateEntry(user, uuid, page, pageSize)
+	response, err := b.CreateEntry(user, uuid)
 	if err != nil {
 		api.ResponseError(c, err)
 		return
 	}
-
 	api.ResponseSuccess(c, response, "")
 }
 
 func (b *baseServer) DeleteModuleEntry(c *gin.Context) {
 	user := c.Request.Header.Get("x-wrapper-username")
 	uuid := c.Query("uuid")
-	page, err := strconv.ParseInt(c.DefaultQuery("page", "1"), 10, 64)
-	if err != nil {
-		api.ResponseError(c, errors.New("page need int type"))
-		return
-	}
-	pageSize, err := strconv.ParseInt(c.DefaultQuery("page_size", "10"), 10, 64)
-	if err != nil {
-		api.ResponseError(c, errors.New("pageSize need int type"))
-		return
-	}
-	response, err := b.DeleteEntry(user, uuid, page, pageSize)
+	response, err := b.DeleteEntry(user, uuid)
 	if err != nil {
 		api.ResponseError(c, err)
 		return
@@ -52,17 +29,7 @@ func (b *baseServer) DeleteModuleEntry(c *gin.Context) {
 
 func (b *baseServer) QueryModuleEntry(c *gin.Context) {
 	user := c.Request.Header.Get("x-wrapper-username")
-	page, err := strconv.ParseInt(c.DefaultQuery("page", "1"), 10, 64)
-	if err != nil {
-		api.ResponseError(c, errors.New("page need int type"))
-		return
-	}
-	pageSize, err := strconv.ParseInt(c.DefaultQuery("page_size", "10"), 10, 64)
-	if err != nil {
-		api.ResponseError(c, errors.New("pageSize need int type"))
-		return
-	}
-	response, err := b.QueryEntry(user, page, pageSize)
+	response, err := b.QueryEntry(user)
 	if err != nil {
 		api.ResponseError(c, err)
 		return
